fb9639e916c3e5bc255c7cf208feda9c5ba3e83d
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2024-06-24 11:06:35 +0200

    iterator over breaking moves

--- ogs/src/game.rs
+++ ogs/src/game.rs
@@ -12,7 +12,7 @@ pub struct Game {
 }
 
 impl Game {
-    fn parse_octal(c: u8) -> Option<u8> {
+    #[inline] fn parse_octal(c: u8) -> Option<u8> {
         (b'0' <= c && c <= b'8').then(|| c - b'0')
     }
 
@@ -51,6 +51,11 @@ impl Game {
             stats.take_option();
         }
     }
+
+    #[inline] pub fn breaking_moves(&self, n: usize) -> BreakingMoveIterator<std::iter::Copied<std::slice::Iter<'_, u8>>> {
+        //BreakingMoveIterator::for_iter(n, self.breaking.iter().copied())
+        BreakingMoveIterator::for_slice(n, self.breaking.as_slice())
+    }
 }
 
 impl FromStr for Game {
@@ -58,5 +63,71 @@ impl FromStr for Game {
     
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Self::from_ascii(s.as_bytes()).ok_or("game description must be in format [X.]OOO... where X is 0 or 4 and (up to 255) Os are octal digits")
+    }
+}
+
+
+pub struct BreakingMoveIterator<I> {
+    current: I,
+    n: usize,
+    after_take: usize,
+    last_i: usize,
+    i: usize
+}
+
+impl<I> BreakingMoveIterator<I> {
+    pub fn for_iter(n: usize, to_take_iterator: I) -> Self {
+        Self { current: to_take_iterator, n, after_take: 0, last_i: 0, i: 0 }
+    }
+}
+
+impl<'a, IU: Into<usize> + Copy> BreakingMoveIterator<std::iter::Copied<std::slice::Iter<'a, IU>>> {
+    pub fn for_slice(n: usize, slice: &'a [IU]) -> Self {
+        Self::for_iter(n, slice.iter().copied())
+    }
+}
+
+impl<IU: Into<usize>, I: Iterator<Item = IU>> Iterator for BreakingMoveIterator<I> {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i == self.last_i {
+            let b = self.current.next()?.into();
+            if b+1 >= self.n { return None; } // n-b >= 2  <=>  !(2 > n-b)  <=>  !(b+2 > n)  <=>  !(b+1 >= n)
+            self.after_take = self.n - b;   // >= 2
+            self.last_i = self.after_take / 2;  // >= 1
+            self.i = 1;
+        } else {
+            self.i += 1;
+        }
+        Some((self.i, self.after_take - self.i))
+    }
+}
+
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::*;
+
+    #[test]
+    fn test_4d() {
+        let g = Game::from_str("4.").unwrap();
+        assert_eq!(g.breaking_moves(0).collect::<Vec<_>>(), vec![]);
+        assert_eq!(g.breaking_moves(1).collect::<Vec<_>>(), vec![]);
+        assert_eq!(g.breaking_moves(2).collect::<Vec<_>>(), vec![(1, 1)]);
+        assert_eq!(g.breaking_moves(3).collect::<Vec<_>>(), vec![(1, 2)]);
+        assert_eq!(g.breaking_moves(4).collect::<Vec<_>>(), vec![(1, 3), (2, 2)]);
+        assert_eq!(g.breaking_moves(5).collect::<Vec<_>>(), vec![(1, 4), (2, 3)]);
+    }
+
+    #[test]
+    fn test_0d44() {
+        let g = Game::from_str("0.44").unwrap();
+        assert_eq!(g.breaking_moves(0).collect::<Vec<_>>(), vec![]);
+        assert_eq!(g.breaking_moves(1).collect::<Vec<_>>(), vec![]);
+        assert_eq!(g.breaking_moves(2).collect::<Vec<_>>(), vec![]);
+        assert_eq!(g.breaking_moves(3).collect::<Vec<_>>(), vec![(1, 1)]);
+        assert_eq!(g.breaking_moves(4).collect::<Vec<_>>(), vec![(1, 2), (1, 1)]);
+        assert_eq!(g.breaking_moves(5).collect::<Vec<_>>(), vec![(1, 3), (2, 2), (1, 2)]);
     }
 }
--- ogs/src/lib.rs
+++ ogs/src/lib.rs
@@ -2,8 +2,10 @@ mod set;
 mod game;
 mod stats;
 
+// solvers:
 mod naive;
 mod rc;
+mod rc2;
 
 pub use set::BitSet;
 pub use game::Game;
